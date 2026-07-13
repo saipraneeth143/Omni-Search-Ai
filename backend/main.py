20e4cbdb4efd3c5da0ec0b4262dfe22447ef7cbd
GOPU PAVAN NAGA VENKATA SAI PRANEETH <gopupraneeth852@gmail.com>
2026-07-13 16:56:07 +0530

    Update import statements for langchain modules

--- backend/main.py
+++ backend/main.py
@@ -11,8 +11,8 @@ from langchain_google_genai import (
     HarmCategory,
     HarmBlockThreshold,
 )
-from langchain.chains import create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_classic.chains import create_retrieval_chain
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 
 # ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
